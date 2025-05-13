a72fd83f2bf46cea5a1593f74140434bc7456fd0
Serge Cage <sergeycage@rocketmail.com>
2025-05-13 23:08:01 +0300

    feat:add test to regform in components

---
+++ src/features/auth/components/tests/regForm-test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { RegForm } from '../regForm';
+
+const TestPage = () => (
+  <div>
+    <h1>Test Registration Form</h1>
+    <RegForm />
+  </div>
+);
+
+export default TestPage;

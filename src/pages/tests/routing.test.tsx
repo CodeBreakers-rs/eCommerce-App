8948d351888028c2d7668cbfb2b562d38940b8d1
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-05-14 16:25:01 -0400

    test: add tests for routing to all pages

--- src/pages/tests/routing.test.tsx
+++ src/pages/tests/routing.test.tsx
@@ -5,6 +5,11 @@ import MainPage from '../main-page'
 import LoginPage from '../login-page'
 import RegistrationPage from '../registration-page'
 import NotFoundPage from '../not-found-page'
+import AboutUsPage from '../about-us-page'
+import BasketPage from '../basket-page'
+import CatalogPage from '../catalog-page'
+import ProfilePage from '../profile-page'
+import ProductPage from '../product-page'
 
 describe('Direct Navigation Routes', () => {
   it('navigates to Main page via direct URL', () => {
@@ -38,6 +43,63 @@ describe('Direct Navigation Routes', () => {
       </MemoryRouter>,
     )
     expect(screen.getByText(/Registration/i)).toBeInTheDocument()
+  })
+
+  it('navigates to About Us page via direct URL', () => {
+    render(
+      <MemoryRouter initialEntries={['/about']}>
+        <Routes>
+          <Route path="/about" element={<AboutUsPage />} />
+        </Routes>
+      </MemoryRouter>,
+    )
+    expect(screen.getByText(/About Us/i)).toBeInTheDocument()
+  })
+
+  it('navigates to Basket page via direct URL', () => {
+    render(
+      <MemoryRouter initialEntries={['/basket']}>
+        <Routes>
+          <Route path="/basket" element={<BasketPage />} />
+        </Routes>
+      </MemoryRouter>,
+    )
+    expect(screen.getByText(/Basket/i)).toBeInTheDocument()
+  })
+
+  it('navigates to Catalog page via direct URL', () => {
+    render(
+      <MemoryRouter initialEntries={['/catalog']}>
+        <Routes>
+          <Route path="/catalog" element={<CatalogPage />} />
+        </Routes>
+      </MemoryRouter>,
+    )
+    expect(screen.getByText(/Catalog/i)).toBeInTheDocument()
+  })
+
+  it('navigates to Profile page via direct URL', () => {
+    render(
+      <MemoryRouter initialEntries={['/profile']}>
+        <Routes>
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </MemoryRouter>,
+    )
+    expect(screen.getByText(/Profile/i)).toBeInTheDocument()
+  })
+
+  it('navigates to Product page with a dynamic ID via direct URL', () => {
+    render(
+      <MemoryRouter initialEntries={['/product/123']}>
+        <Routes>
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </MemoryRouter>,
+    )
+
+    expect(screen.getByText(/Product Page/i)).toBeInTheDocument()
+    expect(screen.getByText(/Product ID: 123/)).toBeInTheDocument()
   })
 })
 describe('Not Found Route', () => {

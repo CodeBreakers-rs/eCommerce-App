ab1f51b96eb8074ff7f2ebcb36fa766689926056
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-05-15 15:08:48 -0400

    chore: remove mock component, related tests

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppRouter from './routes/app-router'
 import App from './app.tsx'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <AppRouter />
       <App />
     </StrictMode>,
   )

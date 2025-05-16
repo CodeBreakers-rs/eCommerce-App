a501459c3a6893bf95f225bffe29b34ea16e25f0
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-05-16 15:33:35 -0400

    feat: update Navigation to conditionally show links

--- src/components/common/navigation.tsx
+++ src/components/common/navigation.tsx
@@ -1,31 +1,43 @@
 import { Link } from 'react-router-dom'
+// import { useAppSelector } from '../../store/hooks'
+
 import './css/navigation.css'
 
 const Navigation = () => {
+  // const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
+  const isLoggedIn = false
   return (
     <nav className="nav">
       <ul className="nav-list">
         <li>
           <Link to="/">🏠 Main</Link>
         </li>
-        <li>
-          <Link to="/login">🔐 Login</Link>
-        </li>
-        <li>
-          <Link to="/register">📝 Register</Link>
-        </li>
-        <li>
-          <Link to="/catalog">📋 Catalog</Link>
-        </li>
-        <li>
-          <Link to="/profile">👤 Profile</Link>
-        </li>
-        <li>
-          <Link to="/basket">🛒 Basket</Link>
-        </li>
-        <li>
-          <Link to="/about">🙋 About</Link>
-        </li>
+        {!isLoggedIn && (
+          <>
+            <li>
+              <Link to="/login">🔐 Login</Link>
+            </li>
+            <li>
+              <Link to="/register">📝 Register</Link>
+            </li>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <li>
+              <Link to="/catalog">📋 Catalog</Link>
+            </li>
+            <li>
+              <Link to="/profile">👤 Profile</Link>
+            </li>
+            <li>
+              <Link to="/basket">🛒 Basket</Link>
+            </li>
+            <li>
+              <Link to="/about">🙋 About</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )

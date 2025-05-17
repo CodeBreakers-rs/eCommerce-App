74aae0f52c517e40eb45652074c5e074984cb72b
Serge Cage <sergeycage@rocketmail.com>
2025-05-17 22:43:24 +0300

    feat: fix htmlfor

--- src/features/auth/components/loginForm.tsx
+++ src/features/auth/components/loginForm.tsx
@@ -64,7 +64,7 @@ export const LoginForm = () => {
           aria-describedby="password-error"
         />
         <div>
-          <label htmlFor={password}>
+          <label htmlFor={'show-password'}>
             <input
               type="checkbox"
               id="show-password"

28a65039853202556273cf388f42016ec5b5257e
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-05-22 23:40:05 -0400

    feat: connects the app to the Redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from 'react-redux'
 import App from './app.tsx'
+import { store } from './store/index.ts'
+import './index.css'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>,
   )
 } else {

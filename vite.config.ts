8082e8bba848c1f94219019afa5781002ffab3e3
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-05-25 01:58:23 -0400

    build: set base path for the deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/eCommerce-App/',
   test: {
     globals: true,
     environment: 'jsdom',

fbab3981433eaa684e9e2688c3a9d14717790fc0
Serge Cage <sergeycage@rocketmail.com>
2025-06-03 18:34:03 +0300

    fix: refactor the message of missing profile

--- src/pages/profile-page.tsx
+++ src/pages/profile-page.tsx
@@ -20,7 +20,11 @@ const ProfilePage = () => {
   }, [customer])
 
   if (!customer)
-    return <div className="text-center text-lg mt-10">Loading profile...</div>
+    return (
+      <div className="text-center text-lg mt-10">
+        No profile data found. You need to log in to view your profile.
+      </div>
+    )
 
   return (
     <div className="max-w-4xl mx-auto p-6">

368b13c2632ae3a78d7a7d203c0f0be94de5b353
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-06-05 22:14:04 -0400

    feat: add fetching products

---
+++ src/features/catalog/services/catalog-service.ts
@@ -0,0 +1,28 @@
+const PROJECT_KEY = import.meta.env.VITE_CT_PROJECT_KEY
+const API_BASE_URL = import.meta.env.VITE_CT_API_URL
+const API_PRODUCTS_SEARCH_URL = `${API_BASE_URL}/${PROJECT_KEY}/product-projections/search`
+
+import type { DessertProduct } from '../../../types/product-list'
+
+const query = new URLSearchParams({
+  staged: 'false',
+  limit: '10',
+}).toString()
+
+export const fetchProducts = async (
+  token: string,
+): Promise<{ results: DessertProduct[] }> => {
+  const response = await fetch(`${API_PRODUCTS_SEARCH_URL}?${query}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    console.log(response)
+    throw new Error(`Failed to fetch products: ${response.status}`)
+  }
+
+  return await response.json()
+}

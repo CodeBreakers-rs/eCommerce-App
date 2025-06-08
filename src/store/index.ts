7cd3798a5ffaa607ded591e0fde035e54ced9141
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-06-08 08:52:09 -0400

    feat: add product to reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer, { authInitialState } from './slices/auth-slice'
 import catalogReducer from './slices/catalog-slice'
+import productReducer from './slices/product-slice'
 import { loadAuthState, saveAuthState } from './local-storage'
 
 const preloadedAuthState = loadAuthState() ?? authInitialState
@@ -9,6 +10,7 @@ export const store = configureStore({
   reducer: {
     auth: authReducer,
     catalog: catalogReducer,
+    product: productReducer,
   },
   preloadedState: {
     auth: preloadedAuthState,

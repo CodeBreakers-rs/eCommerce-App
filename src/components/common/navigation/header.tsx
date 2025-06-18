bd2736e8833df9b5133223a6ed2cb1bdf2faee86
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-06-18 15:09:39 -0400

    refactor: move hamburger-btn inside header

--- src/components/common/navigation/header.tsx
+++ src/components/common/navigation/header.tsx
@@ -2,10 +2,35 @@ import { useState } from 'react'
 import Logo from './logo'
 import NavLinks from './nav-links'
 import UserDropdown from './user-dropdown'
-import HamburgerButton from './hamburger-button'
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const HamburgerButton = () => (
+    <button
+      type="button"
+      onClick={() => setIsMenuOpen((prev) => !prev)}
+      className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-white focus:outline-none focus:ring-2 focus:ring-white transition-transform duration-200 cursor-pointer dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      aria-controls="navbar-user"
+      aria-expanded={isMenuOpen}
+    >
+      <span className="sr-only">Open main menu</span>
+      <svg
+        className="w-5 h-5"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 17 14"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M1 1h15M1 7h15M1 13h15"
+        />
+      </svg>
+    </button>
+  )
 
   return (
     <header className="dark:bg-gray-900">
@@ -14,10 +39,7 @@ const Header = () => {
 
         <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <UserDropdown />
-          <HamburgerButton
-            isMenuOpen={isMenuOpen}
-            onToggle={() => setIsMenuOpen((prev) => !prev)}
-          />
+          <HamburgerButton />
         </div>
 
         <div

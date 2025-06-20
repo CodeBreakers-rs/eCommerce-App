4959e6c395ce8d4a13b5de0d000b2a31977f4ae6
Olga Kharkevich <olga.kharkevich@gmail.com>
2025-06-20 09:45:28 -0400

    chore: fix @typescript-eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,47 +2,46 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 import vitestPlugin from 'eslint-plugin-vitest'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        'vitest/globals': true,
-      },
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default {
+  ignores: ['dist'],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: {
+      ...globals.browser,
+      'vitest/globals': true,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      vitest: vitestPlugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.strictTypeChecked[0].rules,
-      ...reactHooks.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
+    parser: tsParser,
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-)
+  plugins: {
+    '@typescript-eslint': tsPlugin,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    vitest: vitestPlugin,
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...tsPlugin.configs['recommended-requiring-type-checking'].rules,
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+}
